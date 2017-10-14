37539d95bb772c54018a74b8f8f4483edaccbf3e
Manpreet Rajpal <manpreetrajpal7@gmail.com>
2017-10-13 20:50:28 -0400

    first commit

---
+++ PascalsTriangle/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PascalsTriangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(555, 12);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(212, 33);
+            this.btnDisplay.TabIndex = 0;
+            this.btnDisplay.Text = "Display Pascal\'s Triangle";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(13, 84);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(49, 14);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 291);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ PascalsTriangle/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Manpreet Rajpal 
+//September 29, 2017
+//This displays the Pascal's triangle on a 10 by 10 grid
+
+namespace PascalsTriangle
+{
+    public partial class Form1 : Form
+    {
+        int[,] PascalsTriangle; // place holder for the 2D array
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            PascalsTriangle = new int[10, 9]; //10 rows, 10 columns
+            string Output = string.Empty; //""
+
+            for (int i = 0; i < PascalsTriangle.GetLength(0); i++) //rows
+            {
+                for (int j = 0; j < PascalsTriangle.GetLength(1); j++) //columns
+                {
+                    //assigning column 0 and row 0 to 1
+                    if (PascalsTriangle[0, j] == 0)
+                    {
+                        PascalsTriangle[0, j] = 1;
+                    }
+                    else if (PascalsTriangle[i, 0] == 0)
+                    {
+                        PascalsTriangle[i, 0] = 1;
+                    } 
+                    //sets the rest of the numbers by adding the number on left plus number on top
+                    else if (i > 0 | j > 0)
+                    {
+                        PascalsTriangle[i, j] = PascalsTriangle[i - 1, j] + PascalsTriangle[i, j - 1] ;
+                    }
+                }
+            }
+
+            for (int i = 0; i < PascalsTriangle.GetLength(0); i++)
+            { //loop through all the rows
+                Output += "\n";
+
+                for (int j = 0; j < PascalsTriangle.GetLength(1); j++)
+                {
+                    Output += PadSpaces(PascalsTriangle[i, j].ToString(), 10);
+                }
+            }
+
+                lblOutput.Text = Output;
+        }
+         private string PadSpaces(string Word, int ColumnSize)
+        {
+
+            //this method returns a Word (text) padded with 
+            //the number of spaces for a column 
+            string NewWord = string.Empty;
+            int Length = ColumnSize - Word.Length;
+
+            for (int i = 0; i < Length; i++)
+            {
+                NewWord += " ";
+            }
+            NewWord = NewWord + Word;
+
+            return NewWord;
+        }
+        
+    }
+       
+ }
+
